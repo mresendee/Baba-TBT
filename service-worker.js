b2ffeb4011e9a468cc8b9959f3a12fa9326f93a5
mresendee <32345054+mresendee@users.noreply.github.com>
2026-07-17 07:55:46 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pelada-pro-v4';
+const CACHE_NAME = 'pelada-pro-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
